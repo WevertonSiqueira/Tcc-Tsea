188de8f05ee2a4ae3e3481af8711ed13152b0a53
Rique <henriquegoncm@gmail.com>
2026-06-04 12:08:12 -0300

    Fist commit

---
+++ Backend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+import json
+from flask_cors import CORS
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+db = SQLAlchemy(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    # For demonstration, we will use hardcoded credentials
+    if username == 'admin' and password == 'password':
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
+    
+@app.route('/', methods=['GET'])
+def home():
+    return "Ts is running le'go 🖥️🔥🔥🔥"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
